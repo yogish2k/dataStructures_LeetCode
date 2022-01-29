689ef49c11958c11d6435360e5a9fd68b79aa8c7
YOGI <71435868+yogish2k@users.noreply.github.com>
2022-01-29 20:19:37 +0530

    Added sliding window code

---
+++ slidingWIndow.py
@@ -0,0 +1,20 @@
+# slidingWindow
+
+
+def max_sum(arr, windowSize):
+    arraySize = len(arr)
+    if (arraySize <= windowSize):
+        print("Invalid Operation!")
+        return -1
+    window_sum = sum([arr[i] for i in range(windowSize)])
+    max_sum = window_sum
+    for i in range(arraySize - windowSize):
+        window_sum = max_sum - arr[i] + arr[i + windowSize]
+        max_sum = max(window_sum, max_sum)
+    return max_sum
+
+
+arr = [80, -50, 90, 100]
+k = 2
+result = max_sum(arr, k)  # 190
+print(result)
