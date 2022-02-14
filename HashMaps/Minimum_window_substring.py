b21881b631f7c4bac9c9a39d6491854538ada705
YOGI <71435868+yogish2k@users.noreply.github.com>
2022-02-14 23:45:51 +0530

    Minimum Window Substring Using HashMaps
    Signed-off-by: YOGI <71435868+yogish2k@users.noreply.github.com>

---
+++ HashMaps/Minimum_window_substring.py
@@ -0,0 +1,48 @@
+class Solution:
+    def min_window(self, s: str, t: str):
+        len1 = len(s)
+        len2 = len(t)
+
+        if len1 < len2:
+            return ""
+        hashPat = {}
+        hashStr = {}
+
+        for i in range(len2):
+            if hashPat.get(t[i]) is None:
+                hashPat[t[i]] = 0
+            hashPat[t[i]] += 1
+
+        count = 0
+        left = 0
+        startIndex = -1
+        minLen = float("inf")
+
+        for right in range(0, len1):
+            if hashStr.get(s[right]) is None:
+                hashStr[s[right]] = 0
+            hashStr[s[right]] += 1
+
+            if hashPat.get(s[right]) is None:
+                hashPat[s[right]] = 0
+
+            if hashStr.get(s[right]) <= hashPat.get(s[right]):
+                count += 1
+
+            if count == len2:
+                while hashStr.get(s[left]) > hashPat.get(s[left]):
+                    hashStr[s[left]] -= 1
+                    left += 1
+                windowLen = right - left + 1
+                if minLen > windowLen:
+                    minLen = windowLen
+                    startIndex = left
+
+        if startIndex == -1:
+            return "No"
+        return s[startIndex:startIndex + minLen]
+
+
+s = Solution()
+answer = s.min_window('aabcseac', 'ceaad')
+print(answer)
